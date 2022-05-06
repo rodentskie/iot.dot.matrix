eb91991137ef85f88edb68de37871c859aff07e4
rodentskie <rodentskie@gmail.com>
2022-05-07 07:48:10 +0800

    chore: deployments

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+docker rm iot-app -f
+docker rm iot-api -f
+
+docker image rm iotdotmatrix_api:latest
+docker image rm iotdotmatrix_app:latest
+
 docker-compose up -d
 
 echo 'y' | docker image prune
